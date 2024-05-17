1b3c882e9884255df07692b397cf0eed2d4412c3
Drei8 <162891098+Drei8@users.noreply.github.com>
2024-05-18 06:48:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,58 +1,43 @@
 import streamlit as st
 import tensorflow as tf
+from PIL import Image
+import numpy as np
 
-@st.cache_resource
+
+# Load the model
+@st.cache(allow_output_mutation=True)
 def load_model():
-  model=tf.keras.models.load_model('final_model.h5')
-  return model
-model=load_model()
-st.write("""
-# Alzheimer MRI Detection"""
-)
-file=st.file_uploader("Choose an MRI photo from computer",type=["jpg","png"])
+    model = tf.keras.models.load_model("best_model.h5")
+    return model
 
-import cv2
-from PIL import Image,ImageOps
-import numpy as np
 
-# def import_and_predict(image_data,model):
-#     size=(128,128)
-#     image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
-#     img=np.asarray(image)
-#     img_reshape=img[np.newaxis,...]
-#     prediction=model.predict(img_reshape)
-#     return prediction
-
-import cv2
-from PIL import Image, ImageOps
-import numpy as np
-
-def import_and_predict(image_data, model):
-    size = (128, 128)
+def predict(image, model):
+    image = image.resize((192, 192))
+    image = np.array(image) / 255.0
+    image = np.expand_dims(image, axis=0)
     
-    # Resize the image to the expected input shape of the model
-    image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
-    img = np.asarray(image)
-    img = cv2.resize(img, (128, 128), interpolation=cv2.INTER_NEAREST)
+    prediction = model.predict(image)
     
-    # Convert the image to grayscale if necessary
-    if img.ndim == 3 and img.shape[2] == 3:
-        img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-
-    # Reshape the image to add a channel dimension
-    img_reshape = img.reshape((1,) + img.shape + (1,))
-
-    # Make predictions using the Keras model
-    prediction = model.predict(img_reshape)
     return prediction
 
 
-if file is None:
-    st.text("Please upload an image file")
-else:
-    image=Image.open(file)
-    st.image(image,use_column_width=True)
-    prediction=import_and_predict(image,model)
-    class_names=['Mild_Demented', 'Moderate_Demented', 'Non_Demented', 'Very_Mild_Demented']
-    string="OUTPUT : "+ class_names[np.argmax(prediction)]
-    st.success(string)
+def main():
+    st.title("Vehicle Type Classification")
+    st.text("Upload an image of a vehicle to classify it.")
+
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption="Uploaded Image", use_column_width=True)
+
+        model = load_model()
+        prediction = predict(image, model)
+
+        classes = ['bus', 'car', 'motorcycle', 'train', 'truck']  
+        st.subheader("Prediction:")
+        string="OUTPUT : "+classes[np.argmax(prediction)]
+        st.success(string)
+
+if __name__ == "__main__":
+    main()
